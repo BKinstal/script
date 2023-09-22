bcf919e77a476f7755f071d452a01b2bf24223d7
BKinstal <143218188+BKinstal@users.noreply.github.com>
2023-09-23 00:04:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Stáhněte preseed konfiguraci
-wget https://raw.githubusercontent.com/yourusername/yourrepo/master/preseed.cfg
+wget https://raw.githubusercontent.com/BKinstal/script/main/preseed.cfg
 
 # Spusťte instalaci Proxmoxu s preseed konfigurací
 debian-installer-launcher -preseed /path/to/preseed.cfg
